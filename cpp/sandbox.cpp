e6bd321d573036244493be1a7501a7184b003deb
karim <karimelsisi27@gmail.com>
2022-09-16 09:50:55 +0200

    circle area problem

--- cpp/sandbox.cpp
+++ cpp/sandbox.cpp
@@ -1,36 +1,15 @@
-#include <iostream>
-#include <map>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
     
-    vector<string> ops = {"5", "6", "+", "9", "D", "3", "C"};
+    double pi = 3.141592653;
+    double r;
     
-    int sum = 0;
+    cin >> r;
 
-    
+    double area = pi * (r * r);
 
-    vector<int> result;
 
-    for(int i = 0; i != ops.size(); ++i){
-        
-        if(ops[i] == "+"){
-            result.push_back(stoi(ops[i - 1]) + stoi(ops[i - 2]));
-        }
-
-        if(ops[i] == "D"){
-            result.push_back(stoi(ops[i - 1]) * 2);
-        }
-
-        
-    }
-
-    for(auto i: result){
-        sum+= i;
-    }
-
-    cout << sum << endl;
-    return 0;
-}
-
+    cout << fixed << setprecision(9) << area << endl;
+} 
