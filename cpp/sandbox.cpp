497b0ebca5c9cb1e4fb4d5761f93ef483c9abec0
karim <karimelsisi27@gmail.com>
2022-09-19 22:13:39 +0200

    Age in days problem

--- cpp/sandbox.cpp
+++ cpp/sandbox.cpp
@@ -4,16 +4,24 @@ using namespace std;
 
 int main(){
 
-    float x, y;
+    int age, years, months, days;
 
-    cin >> x >> y;
+    cin >> age;
 
-    if(x > 0.0 && y > 0.0) cout << "Q1\n";
-    if(x < 0.0 && y > 0.0) cout << "Q2\n";
-    if(x < 0.0 && y < 0.0) cout << "Q3\n";
-    if(x > 0.0 && y < 0.0) cout << "Q4\n";
-    if(x == 0.0 && y == 0.0) cout << "Origem\n";
-    if(y == 0.0 && x != 0.0) cout << "Eixo X\n";
-    if(x == 0.0 && y != 0.0) cout << "Eixo Y\n";
+    years = age / 365;
+
+    age -= (years * 365);
+
+    months = age / 30;
+
+    age -= (months * 30);
+
+    days = age;
+
+    cout << years << " years\n";
+    cout << months << " months\n";
+    cout << days << " days\n";
+    
+
     
 } 
