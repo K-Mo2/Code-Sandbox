eee310d37656e72dfb6b71bc3bf1c9f0d6939107
karim <karimelsisi27@gmail.com>
2022-09-21 21:05:14 +0200

    Mathematical expression problem

--- cpp/sandbox.cpp
+++ cpp/sandbox.cpp
@@ -4,20 +4,22 @@ using namespace std;
 
 int main(){
 
-    int a,b;
-    char s;
+    int a,b, c;
+    char s, q;
 
-    cin >> a >> s >> b;
-
-    if(s == '>'){
-        if(a > b) cout << "Right\n"; else cout << "Wrong\n";
+    cin >> a >> s >> b >> q >> c;
+    
+    if(q != '=') return 0;
+    
+    if(s == '+'){
+        if(a + b == c) cout << "Yes\n"; else cout << a + b << endl;
     }
 
-    if(s == '<'){
-        if(a < b) cout << "Right\n"; else cout << "Wrong\n";
+    if(s == '-'){
+        if(a - b == c) cout << "Yes\n"; else cout << a - b << endl;
     }
 
-    if(s == '='){
-        if(a == b) cout << "Right\n"; else cout << "Wrong\n";
+    if(s == '*'){
+        if(a * b == c) cout << "Yes\n"; else cout << a * b << endl;
     }
 } 
