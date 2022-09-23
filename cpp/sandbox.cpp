a697a185a47201a2f28882e76ce90d94b32cc268
karim <karimelsisi27@gmail.com>
2022-09-23 21:13:24 +0200

    Next Alphabet problem

--- cpp/sandbox.cpp
+++ cpp/sandbox.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 int main(){
 
-    long long a, b, k;
+    char c;
 
-    cin >> a >> b >> k;
+    cin >> c;
 
-    if(a % k == 0 && b % k != 0) cout << "Memo\n";
-    else if(b % k == 0 && a % k != 0) cout << "Momo\n";
-    else if(a % k == 0 && b % k == 0) cout << "Both\n";
-    else if(a % k != 0 && b % k != 0) cout << "No One\n";
-    return 0;
+    if(c == 122){
+        cout << (char)(c - 25) << endl;
+    } else {
+        cout << (char)(c + 1) << endl;
+    }
 
 } 
