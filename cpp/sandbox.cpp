6c235904b70e00d5fa21ce36f774a5c8761387df
karim <karimelsisi27@gmail.com>
2022-09-29 18:25:08 +0200

    Adding Bits problem

--- cpp/sandbox.cpp
+++ cpp/sandbox.cpp
@@ -2,26 +2,12 @@
     #include <string>
     using namespace std;
 
-    void decToBin(int n){
-
-        int binArr[32];
-        int i = 0;
-
-        while(n > 0){
-            binArr[i] = n % 2;
-            n /= 2;
-            i++;
-        }
-
-        for(int j = i - 1; j >= 0; j--){
-            cout << binArr[j];
-        }
-    }
-
     int main(){
-        int n;
-        cin >> n;
-        decToBin(n);
-        cout << endl;   
+
+        unsigned int x, y;
+
+        cin >> x >> y;
+
+        cout << (x ^ y) << endl;
         return 0; 
     } 
