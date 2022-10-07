e9dc2bacc48a7ddfe536da6acb476cf966c4e3ff
karim <karimelsisi27@gmail.com>
2022-10-07 23:46:13 +0200

    1 to N problem

--- cpp/sandbox.cpp
+++ cpp/sandbox.cpp
@@ -2,23 +2,10 @@
     using namespace std;
 
     int main(){
-
-        int n, d1, d2;
+        int n;
 
         cin >> n;
 
-        d1 = n % 10;
-        n /= 10;
-
-        d2 = n % 10;
-        n /= 10;
-
-        if(d1 % d2 == 0){
-            cout << "YES\n";
-        } else if(d2 % d1 == 0){
-            cout << "YES\n";
-        } else {
-            cout << "NO\n";
-        }
+        for(int i = 1; i <= n; i++) cout << i << endl;
         return 0;
     }
