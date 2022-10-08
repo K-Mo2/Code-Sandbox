33d9763d1b9c429d416849a8a2471edc3bba00aa
karim <karimelsisi27@gmail.com>
2022-10-08 10:20:12 +0200

    Even, Odd, Positive, Negative

--- cpp/sandbox.cpp
+++ cpp/sandbox.cpp
@@ -2,21 +2,24 @@
     using namespace std;
 
     int main(){
-        int n;
+
+        int n=0, pos=0, neg=0, odd=0, even=0, x;
 
         cin >> n;
-        
-        if(n == 1){
-            cout << -1 << endl;
-            return 0;
-        }
-        if(n % 2 != 0){
-           n -= 1;
-        } 
 
-        for(int i = 2; i <= n; i+=2) {
-            cout << i << endl;
-        }
-        
+        while(n){
+
+            cin >> x;
+
+            if(x > 0)pos++;                
+            if(x < 0)neg++;
+            if(x % 2 == 0)even++;
+            if(x % 2 != 0)odd++;
+            n--;
+        } 
+        cout << "Even: " << even << endl;  
+        cout << "Odd: " << odd << endl;  
+        cout << "Positive: " << pos << endl;  
+        cout << "Negative: " << neg << endl;  
         return 0;
     }
