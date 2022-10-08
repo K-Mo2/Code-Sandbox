cf26d76ee4ca66a508b5f5b8aa9167ddba1c51df
karim <karimelsisi27@gmail.com>
2022-10-08 10:27:18 +0200

    Fixed Password problem

--- cpp/sandbox.cpp
+++ cpp/sandbox.cpp
@@ -2,24 +2,18 @@
     using namespace std;
 
     int main(){
+        int x;
 
-        int n=0, pos=0, neg=0, odd=0, even=0, x;
-
-        cin >> n;
-
-        while(n){
-
+        while (true)
+        {
             cin >> x;
-
-            if(x > 0)pos++;                
-            if(x < 0)neg++;
-            if(x % 2 == 0)even++;
-            if(x % 2 != 0)odd++;
-            n--;
-        } 
-        cout << "Even: " << even << endl;  
-        cout << "Odd: " << odd << endl;  
-        cout << "Positive: " << pos << endl;  
-        cout << "Negative: " << neg << endl;  
+            if(x == 1999){
+                cout << "Correct\n";
+                return 0;
+            } else {
+                cout << "Wrong\n";
+            }
+        }
+        
         return 0;
     }
