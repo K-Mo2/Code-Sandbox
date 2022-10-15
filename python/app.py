0c5b6f5678d30652887b22fe486476fdcc591190
karim <karimelsisi27@gmail.com>
2022-10-15 23:29:04 +0200

    Get outer digits with python

--- python/app.py
+++ python/app.py
@@ -1,24 +1,34 @@
 
-ops = ["5", "6", "+", "9", "D", "3", "C"]
+# ops = ["5", "6", "+", "9", "D", "3", "C"]
 
-def operations(ops):
+# def operations(ops):
     
-    result = []
-    Sum = 0
+#     result = []
+#     Sum = 0
 
-    for i in range(0, len(ops)):
+#     for i in range(0, len(ops)):
 
-        if ops[i] == "+":
-            result.append((int(ops[i - 1]) + int(ops[i - 2])))
+#         if ops[i] == "+":
+#             result.append((int(ops[i - 1]) + int(ops[i - 2])))
 
-        if ops[i] == "D":
-            result.append((int(ops[i - 1]) * 2))
+#         if ops[i] == "D":
+#             result.append((int(ops[i - 1]) * 2))
 
-        if ops[i] == "C":
-            result.append(0)
+#         if ops[i] == "C":
+#             result.append(0)
 
-    Sum = sum(result)
+#     Sum = sum(result)
 
-    return Sum
+#     return Sum
 
-print(operations(ops))
+# print(operations(ops))
+
+def get_outer_digits():
+    x = int(input())
+    num_len = len(str(x))
+    ld = x % 10
+    fd = int(x / pow(10, num_len - 1))
+    print("FD: ", fd)
+    print("LD: ", ld)
+
+get_outer_digits()
