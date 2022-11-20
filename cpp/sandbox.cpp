566e8b70467df64c98d2b1131886c17fa13e2835
karim <karimelsisi27@gmail.com>
2022-11-20 21:51:34 +0200

    Weird Algorithm problem

--- cpp/sandbox.cpp
+++ cpp/sandbox.cpp
@@ -521,9 +521,25 @@ using namespace std;
 
     }
 
+    void weird_algorithm(){
+        long long n;
+
+        cin >> n;
+        while(n > 1){
+
+            cout << n << " ";
+
+            if(n % 2 ==0) n /= 2;
+            else  n = (n * 3) + 1;
+
+        }
+            cout << n;
+            cout << endl;
+    }
+
     int main(){
         
-        three_nums();
+        repetition();
 
         return 0;
     }
