36bc27864a5eb66d30b055c3015692350c3944c6
Karim Elsisi <karimelsisi27@gmail.com>
2022-12-05 11:54:01 +0200

    U.Is B a sequence of A

--- cpp/sandbox.cpp
+++ cpp/sandbox.cpp
@@ -880,9 +880,37 @@ using namespace std;
         
     }
 
+    void is_b_sequence_of_a(){
+        unsigned int n, m;
+
+        cin >> n >> m;
+
+        long long a[n], b[m];
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        for (int i = 0; i < m; i++)
+        {
+            cin >> b[i];
+        }
+
+        unsigned int sI=0;
+        for (int i = 0; i < n; i++)
+        {
+            if(a[i] == b[sI]) sI++;
+        }
+
+        (sI == m) ? cout << "YES\n" : cout << "NO\n";
+        
+    }
+    
+    
     int main(){
         
-        permutation_arrays();
+        is_b_sequence_of_a();
 
         return 0;
     }
