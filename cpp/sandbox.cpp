6a0c9a693e36c732f7341cd52e1197c400362418
Karim Elsisi <karimelsisi27@gmail.com>
2022-12-06 23:56:37 +0200

    W.Mirror Array problem

--- cpp/sandbox.cpp
+++ cpp/sandbox.cpp
@@ -930,10 +930,36 @@ using namespace std;
         
         
     }
+
+    void mirror_array(){
+        int n, m;
+
+        cin >> n >> m;
+
+        unsigned int arr[n][m], mirror_arr[n][m];
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                cin >> arr[i][j];
+            }
+            
+        }
+        
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = m-1; j >= 0; j--)
+            {
+                cout << arr[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
     
     int main(){
         
-        frequency_array();
+        mirror_array();
 
         return 0;
     }
