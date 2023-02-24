fae271fb93b1d592007ea0988e4ee0a6e25b040b
Karim Elsisi <karimelsisi27@gmail.com>
2023-02-25 01:13:13 +0200

    Recursion: J.Factorial problem

--- cpp/sandbox.cpp
+++ cpp/sandbox.cpp
@@ -1,36 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string even_indices(int arr[], string s2, int len, int i){
-    
-    if(i < 0){
-        return s2;
+long long factorial(long long n){
+    if(n <= 1){
+        return n;
     }
-    if(i % 2 == 0){
-        cout << arr[i] << " ";
-    }
-    return even_indices(arr, s2, len, i - 1);
+
+    return n * factorial(n - 1);
 }
-
-int* recurrence(int n, int i, int arr[]){
-    
-
-    if(i >= n){
-        return arr;
-    }
-    
-    cin >> arr[i];
-    
-    return recurrence(n, i + 1, arr);
-} 
-
     
 int main(){
 
-    int n;
+    long long n;
     cin >> n; 
-    int arr[n];
-    int* res = recurrence(n, 0, arr);
-    cout << even_indices(res, "", n, n - 1) << endl;
+    cout << factorial(n) << endl;
     return 0;
 }
