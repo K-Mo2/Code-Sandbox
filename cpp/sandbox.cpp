d36b0b31f912b05000379f10c83b1ad5210a0460
Karim Elsisi <karimelsisi27@gmail.com>
2023-03-31 06:16:51 +0200

    Valid Parenthesis problem

--- cpp/sandbox.cpp
+++ cpp/sandbox.cpp
@@ -1,32 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 
-int romanToInt(string s){
+bool isValid(string s){
     
-    int sum = 0;
-    int n = s.size();
-
-    unordered_map<char, int> mp{
-        {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}
+    unordered_map<char, char> umap = {
+        {'(', ')'},
+        {'[', ']'},
+        {'{', '}'},
     };
-
-    for (int i = 0; i < n; i++)
+    
+    stack<char> st;
+    int len = s.size();
+    
+    for (int i = 0; i < len; i++)
     {
-        if(mp[s[i]] < mp[s[i+1]]) sum -= mp[s[i]];
+        char c = s[i];
+
+        if(c == '(' || c == '[' || c == '{'){
+            
+            st.push(c);
+            
+        } else {
+            
+            if(st.empty()){
+            
+                return false;
+            
+            } else {
+                
+                char top = st.top();
+                
+                if(c == umap[top]){
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
         
-        if(mp[s[i]] >= mp[s[i+1]]) sum += mp[s[i]];
     }
     
-    return sum;
+    
+    if (st.empty())
+    {
+        return true;
+    } 
+    
+    return false;
 }
 
 int main(){
     
     string s;
+    
     cin >> s;
 
-    cout << romanToInt(s) << endl;
-
+    isValid(s) ? cout << "true\n" : cout << "false\n";
     return 0;
 }
